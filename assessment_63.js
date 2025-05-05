92b3d1babdf7040d4bf67bf60556427d0a42d6ee
Hutesh Vidhate <huteshvidhate23@gmail.com>
2025-05-05 15:53:02 +0530

    assessment

---
+++ assessment_63.js
@@ -0,0 +1,19 @@
+function getUniqueSortedDomains(emails) {
+    return Array.from(
+      new Set(
+        emails.map(email => email.split('@')[1]) // extract domain
+      )
+    ).sort(); // sort the unique domains alphabetically
+  }
+
+  const emails = [
+    "user1@gmail.com",
+    "user2@yahoo.com",
+    "user3@gmail.com",
+    "user4@outlook.com",
+    "user5@yahoo.com",
+    "user6@gmail.com"
+  ];
+  
+  console.log(getUniqueSortedDomains(emails));
+  // Output: ["gmail.com", "outlook.com", "yahoo.com"]
